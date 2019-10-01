99795802492caca0ef9cb5b7c80dcec8c1ad1063
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-09-30 20:01:14 -0400

    added and connected db

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,40 +1,38 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-let links = [{
-  id: 'link-0',
-  url: 'wwww.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+// let links = [{
+//   id: 'link-0',
+//   url: 'wwww.howtographql.com',
+//   description: 'Fullstack tutorial for GraphQL'
+// }]
 
-let idCount = links.length
+// let idCount = links.length
 
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
-    feed: () => links,
-    link: (parent, args) => links.find(el => el.id === args.id)
+    feed: (root, args, context, info) => context.prisma.links(),
+    // link: (parent, args) => links.find(el => el.id === args.id)
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-      links.push(link)
-      return link
+    post: (parent, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      })
     },
-    updateLink: (parent, args) => {
-      let link = links.find(el => el.id === args.id)
-      link.url = args.url ? args.url : link.url
-      link.description = args.description ? args.description: link.description
-      return link
-    },
-    deleteLink: (parent, args) => {
-      let link = links.find(el => el.id === args.id)
-      links = links.filter(el => el.id !== args.id)
-      return link
-    }
+    // updateLink: (parent, args) => {
+    //   let link = links.find(el => el.id === args.id)
+    //   link.url = args.url ? args.url : link.url
+    //   link.description = args.description ? args.description: link.description
+    //   return link
+    // },
+    // deleteLink: (parent, args) => {
+    //   let link = links.find(el => el.id === args.id)
+    //   links = links.filter(el => el.id !== args.id)
+    //   return link
+    // }
   },
   // Link: {
   //   id: (parent) => parent.id,
@@ -45,7 +43,8 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma }
 })
 
 server.start(() => console.log('Server is running on PORT 4000'))
