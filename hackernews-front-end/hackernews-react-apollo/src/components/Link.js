ee42dc3a17d64d5238c537690ad307ee3930b32e
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-12-30 16:14:00 -0300

    done updating cache after mutation

--- hackernews-front-end/hackernews-react-apollo/src/components/Link.js
+++ hackernews-front-end/hackernews-react-apollo/src/components/Link.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
+import { AUTH_TOKEN } from './constants';
+import { timeDifferenceForDate } from '../utils';
+import { gql } from 'apollo-boost';
+import { Mutation } from 'react-apollo';
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkID: ID!) {
+    vote(linkId: $linkID) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
 
 class Link extends Component {
   render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN);
     return (
-      <div>
-        <div>
-          {this.props.link.description} ({this.props.link.url})
+      <div className="flex mt2 items-start">
+        <div className="flex items-center">
+          <span className="gray">{this.props.index + 1}.</span>
+          {authToken && (
+            <Mutation
+              mutation={VOTE_MUTATION}
+              update={(store, { data }) => {
+                console.log('store:', store);
+                console.log('data', data);
+                return this.props.updateStoreAfterVote(
+                  store,
+                  data.vote,
+                  this.props.link.id
+                );
+              }}
+              variables={{ linkID: this.props.link.id }}
+            >
+              {voteMutation => (
+                <div className="ml1 gray f11 pointer" onClick={voteMutation}>
+                  ▲
+                </div>
+              )}
+            </Mutation>
+          )}
+        </div>
+        <div className="ml1">
+          <div>
+            {this.props.link.description} ({this.props.link.url})
+          </div>
+          <div className="f6 lh-copy gray">
+            {this.props.link.votes.length} votes | by{' '}
+            {this.props.link.postedBy
+              ? this.props.link.postedBy.name
+              : 'unknown'}{' '}
+            {timeDifferenceForDate(this.props.link.createdAt)}
+          </div>
         </div>
       </div>
     );
--- hackernews-front-end/hackernews-react-apollo/src/components/LinkList.js
+++ hackernews-front-end/hackernews-react-apollo/src/components/LinkList.js
@@ -3,19 +3,38 @@ import Link from './Link';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
         id
         url
         description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `;
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+    const votedLink = data.feed.links.find(link => linkId === link.id);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+
   render() {
     return (
       <Query query={FEED_QUERY}>
@@ -25,7 +44,14 @@ class LinkList extends Component {
 
           const linksToRender = data.feed.links;
 
-          return linksToRender.map(link => <Link key={link.id} link={link} />);
+          return linksToRender.map((link, i) => (
+            <Link
+              key={link.id}
+              link={link}
+              index={i}
+              updateStoreAfterVote={this._updateCacheAfterVote}
+            />
+          ));
         }}
       </Query>
     );
