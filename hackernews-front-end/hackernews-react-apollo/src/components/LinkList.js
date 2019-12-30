a6282d4f5a17b4c98c0faf050ee3506b6221f433
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-12-30 19:33:40 -0300

    real time updates done

--- hackernews-front-end/hackernews-react-apollo/src/components/LinkList.js
+++ hackernews-front-end/hackernews-react-apollo/src/components/LinkList.js
@@ -26,8 +26,57 @@ export const FEED_QUERY = gql`
   }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
 class LinkList extends Component {
   _updateCacheAfterVote = (store, createVote, linkId) => {
+    console.log('but I ran');
     const data = store.readQuery({ query: FEED_QUERY });
     const votedLink = data.feed.links.find(link => linkId === link.id);
     votedLink.votes = createVote.link.votes;
@@ -35,15 +84,45 @@ class LinkList extends Component {
     store.writeQuery({ query: FEED_QUERY, data });
   };
 
+  _subscribeToNewLinks = async subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        console.log('I RAN!');
+        if (!subscriptionData.data) return prev;
+        const newLink = subscriptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename,
+          },
+        });
+      },
+    });
+  };
+
+  _subscribeToNewVotes = async subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION,
+    });
+  };
+
   render() {
     return (
       <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div>Fetching</div>;
           if (error) return <div>Error</div>;
 
-          const linksToRender = data.feed.links;
+          this._subscribeToNewLinks(subscribeToMore);
+          this._subscribeToNewVotes(subscribeToMore);
 
+          const linksToRender = data.feed.links;
+          console.log('do I run?');
+          console.log('links', linksToRender);
           return linksToRender.map((link, i) => (
             <Link
               key={link.id}
